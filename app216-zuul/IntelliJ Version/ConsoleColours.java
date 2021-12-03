53d46eed4d2ef3a48c68971e163f21f0f219ac08
AlbertCoyle27 <91730103+AlbertCoyle27@users.noreply.github.com>
2021-12-03 11:36:18 +0000

    Established App21-06 as IntelliJ Project
    Work on this app will be undertaken using IntelliJ rather than BlueJ.

---
+++ app216-zuul/IntelliJ Version/ConsoleColours.java
@@ -0,0 +1,10 @@
+/**
+ * Allows for the manipulation of colour display via console input. This cannot be done in BlueJ due
+ * to software limitations.
+ * @author Albert Coyle
+ * @version 03/12/2021
+ */
+public class ConsoleColours
+{
+
+}
